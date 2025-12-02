d3be185757de4fdc29fb9075d465827b5ec9244c
tierracero <vcantu01@tierracero.com>
2025-12-01 22:25:31 -0600

    minor fix

--- Sources/App/Snippits/CustConcession/CustConcessionView.swift
+++ Sources/App/Snippits/CustConcession/CustConcessionView.swift
@@ -1469,7 +1469,7 @@ class CustConcessionView: Div {
                 itemsPOCRefrence[pocId] = newItems
                 
             }
-
+            
             self.itemsPOCRefrence = itemsPOCRefrence
 
             // MARK: Top level ciontainer, transition 
